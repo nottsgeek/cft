09e18244c519081215de2eebbd49b24c1c274e24
Ashish <ashishinnovate@gmail.com>
2018-09-07 17:57:25 +0530

    news

---
+++ applycft.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import boto3
+import sys
+s3_client = boto3.client('s3')
+
+# Upload the file to S3
+bucket_url="https://s3-us-west-2.amazonaws.com/templateash/"
+cft_file=sys.argv[1]
+cft_url=bucket_url + cft_file
+stack=sys.arv[2]
+print cft_file
+print cft_url
+s3_client.upload_file(cft_file, 'templateash', cft_file)
+cft_client = boto3.client('cloudformation')
+response = cft_client.create_stack(
+    StackName=stack,
+    TemplateURL=cft_url,
+    DisableRollback=True,
+    ClientRequestToken='string'
+)
+print response
